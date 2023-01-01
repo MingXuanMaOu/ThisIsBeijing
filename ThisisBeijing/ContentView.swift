0c1f1371f02518b0789d4c0a6d3bfde31fde52ab
MingXuMaOu <92174693+MingXuMaOu@users.noreply.github.com>
2023-01-01 20:29:52 +0800

    Initial Commit

---
+++ ThisisBeijing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThisisBeijing
+//
+//  Created by 刘铭 on 2023/1/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThisisBeijing/ThisisBeijingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThisisBeijingApp.swift
+//  ThisisBeijing
+//
+//  Created by 刘铭 on 2023/1/1.
+//
+
+import SwiftUI
+
+@main
+struct ThisisBeijingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
